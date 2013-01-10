d48b86ffa29bf7bcebc5f902c6cd412beaeb5994
Pagge <Patrick.Ivarsson@gmail.com>
2013-01-10 01:58:24 +0100

    todo

---
+++ src/com/example/testapp/TODO.java
@@ -0,0 +1,11 @@
+package com.example.testapp;
+
+public class TODO {
+	/*
+	
+		TODO: Gör news stroken ljusare i färgen (och kanske streckad)
+		TODO: Kolla så att inga uppkopplingar ligger igång efter att rss är hämtad
+		TODO: Fixa så att appen inte krashar om man saknar internetuppkoppling.
+	 
+	*/
+}
