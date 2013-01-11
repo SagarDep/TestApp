e9f912e89259ebe278dbcd3ba4d6a493b137af7e
Pagge <Patrick.Ivarsson@gmail.com>
2013-01-11 19:40:25 +0100

    TODO

--- src/com/example/testapp/TODO.java
+++ src/com/example/testapp/TODO.java
@@ -3,9 +3,6 @@ package com.example.testapp;
 public class TODO {
 	/*
 	 	
-	 	FIXME: Lägg till plats till varje tid om det går typ skriv "10:00-12:00 Brännboll | Sankt Hans Backar"
-	
-		TODO: Gör news stroken ljusare i färgen (och kanske streckad)
 		TODO: Kolla så att inga uppkopplingar ligger igång efter att rss är hämtad
 		TODO: Fixa så att appen inte krashar om man saknar internetuppkoppling.
 	 	TODO: Gör 9 patch knappar för focus och focus/pressed
@@ -13,5 +10,18 @@ public class TODO {
 	 	TODO: Gör så att man kan refresha nyheter utan att gå ut och tillbaka
 	 	TODO: Ändra EMSG_NO_INTERNET_CONNECTION
 	 	
+	 	(SQLite är kanske lite overkill för denna appen, kommer inte innehålla några större mängder data)
+	 	Kan spara ner ArrayList som Set och sen när man tar tillbaka de gör man en ny ArrayList och sorterar
+	 	Kan tex spara ner i-värdet i varje post eller liknande för att sen kunna sortera de när de e i set
+	 	//Retrieve the values
+		Set<String> set = new HashSet<String>();
+		set = myScores.getStringSet("key", null);
+		
+		//Set the values
+		Set<String> set = new HashSet<String>();
+		set.addAll(listOfExistingScores);
+		scoreEditor.putStringSet("key", set);
+		scoreEditor.commit();
+
 	*/
 }
