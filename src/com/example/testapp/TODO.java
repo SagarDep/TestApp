bf075d375eef182eabee11a49fe2e39ccb1999f7
Pagge <Patrick.Ivarsson@gmail.com>
2013-01-12 12:00:09 +0100

    TODO

--- src/com/example/testapp/TODO.java
+++ src/com/example/testapp/TODO.java
@@ -6,9 +6,15 @@ public class TODO {
 		TODO: Kolla så att inga uppkopplingar ligger igång efter att rss är hämtad
 		TODO: Fixa så att appen inte krashar om man saknar internetuppkoppling.
 	 	TODO: Gör 9 patch knappar för focus och focus/pressed
-	 	TODO: Spara ner de senaste nerladdade nyheterna så om uppkoppling saknas läses de gamla in.
-	 	TODO: Gör så att man kan refresha nyheter utan att gå ut och tillbaka
 	 	TODO: Ändra EMSG_NO_INTERNET_CONNECTION
+	 	
+	 	
+	 	//LADDA INTE NER FÖR MYCKET
+	 	TODO: 1. Kolla om den cachade datan är äldre än en viss tid och om det är fallet uppdatera den (typ 5minuter eller nåt) + punkt 3
+	 	TODO: 2. Spara ner de senaste nerladdade nyheterna så om uppkoppling saknas läses de gamla in.
+	 	TODO: 3. Gör så att man kan refresha nyheter utan att gå ut och tillbaka
+
+	 	
 	 	
 	 	(SQLite är kanske lite overkill för denna appen, kommer inte innehålla några större mängder data)
 	 	Kan spara ner ArrayList som Set och sen när man tar tillbaka de gör man en ny ArrayList och sorterar
