bd422bedd236a964fd58dc607faf2128a8f3708f
Pagge <Patrick.Ivarsson@gmail.com>
2013-01-13 16:50:51 +0100

    TODO

--- src/com/example/testapp/TODO.java
+++ src/com/example/testapp/TODO.java
@@ -6,22 +6,12 @@ public class TODO {
 	 	TODO: Gör 9 patch knappar för focus och focus/pressed
 	 	
 	 	//LADDA INTE NER FÖR MYCKET
-	 	TODO: 1. Kolla om den cachade datan är äldre än en viss tid och om det är fallet uppdatera den (typ 5minuter eller nåt) + punkt 3
-	 	TODO: 2. Spara ner de senaste nerladdade nyheterna så om uppkoppling saknas läses de gamla in.
+	 	TODO: DONE! 1. Kolla om den cachade datan är äldre än en viss tid och om det är fallet uppdatera den (typ 5minuter eller nåt) + punkt 3
+	 	TODO: DONE! 2. Spara ner de senaste nerladdade nyheterna så om uppkoppling saknas läses de gamla in.
 	 	TODO: 3. Gör så att man kan refresha nyheter utan att gå ut och tillbaka
 
-	 	(SQLite är kanske lite overkill för denna appen, kommer inte innehålla några större mängder data)
-	 	Kan spara ner ArrayList som Set och sen när man tar tillbaka de gör man en ny ArrayList och sorterar
-	 	Kan tex spara ner i-värdet i varje post eller liknande för att sen kunna sortera de när de e i set
-	 	//Retrieve the values
-		Set<String> set = new HashSet<String>();
-		set = myScores.getStringSet("key", null);
-		
-		//Set the values
-		Set<String> set = new HashSet<String>();
-		set.addAll(listOfExistingScores);
-		scoreEditor.putStringSet("key", set);
-		scoreEditor.commit();
 
+		IDEA: Läs in allt på en gång när appen startar. Kör en diff mellan en sparad Array och en nyss nerladdade och notera på
+			  knappen till tex nyheter att det har hänt nåt nytt.
 	*/
 }
