fda2a7653b66c0239bb966d25643cd2aa74812b3
Pagge <Patrick.Ivarsson@gmail.com>
2013-01-16 22:34:01 +0100

    Map using DB for markers

--- src/com/example/testapp/Map.java
+++ src/com/example/testapp/Map.java
@@ -1,11 +1,30 @@
 package com.example.testapp;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -14,6 +33,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMap.InfoWindowAdapter;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
@@ -21,6 +41,15 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class Map extends FragmentActivity {
+	private static BitmapDescriptor MARKER_HOSPITAL;
+	private static BitmapDescriptor MARKER_MED;
+	private static BitmapDescriptor MARKER_TRAIN;
+	private static BitmapDescriptor MARKER_STORE;
+	private static BitmapDescriptor MARKER_FASTFOOD;
+	
+	private static HashMap<String, BitmapDescriptor> markMap;
+	private static ArrayList<CustomMarker> markers = null;
+	
 	private final LatLng POS_LUND = new LatLng(55.704580, 13.190632);
 	private GoogleMap map;
 	
@@ -30,19 +59,24 @@ public class Map extends FragmentActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_map);
-
-
+		
+		
 		this.map = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
 		map.setMyLocationEnabled(true);
-		map.addMarker(new MarkerOptions().position(new LatLng(55.711011, 13.210523)).title("E-Huset").icon(BitmapDescriptorFactory.fromResource(R.drawable.rightarrow)));
-		map.addMarker(new MarkerOptions().position(new LatLng(55.709597, 13.210566)).title("M-Huset"));
-		map.addMarker(new MarkerOptions().position(new LatLng(55.712402, 13.209150)).title("Kårhuset"));
+		
+		String content = connectToDB();
+		markers = getMarkers(content);
 
+		initMarkerIcons();
+		addMarkers();
+		
+//		initMarkers();
+		
 		//TEST AV INFO WINDOW
 		map.setInfoWindowAdapter(new InfoWindowAdapter(){
 
 			@Override
-			public View getInfoContents(Marker arg0) {
+			public View getInfoContents(Marker marker) {
 				View v = getLayoutInflater().inflate(R.layout.map_info, null);
 				
 				ImageView im = (ImageView) v.findViewById(R.id.map_info_icon);
@@ -51,7 +85,7 @@ public class Map extends FragmentActivity {
 				TextView tv3 = (TextView) v.findViewById(R.id.map_info_desc);
 				
 				im.setImageResource(R.drawable.pink_square_big);
-				tv1.setText("E-Huset");
+				tv1.setText(marker.getTitle());
 				tv2.setText("von Scheelevägen 11");
 				tv3.setText("Where all the true nerds go to party. Where all the true nerds go to party. Where all the true nerds go to party. ");
 				
@@ -98,9 +132,96 @@ public class Map extends FragmentActivity {
 //		}
 	}
 	
+	private void addMarkers() {
+		if(markers != null) {
+			for (CustomMarker m : markers) {
+				map.addMarker(new MarkerOptions().position(new LatLng(m.lat, m.lng)).title(m.title).icon(markMap.get(m.cat)));
+			}
+		}
+	}
+
+	private ArrayList<CustomMarker> getMarkers(String content) {
+		
+		try {
+			Log.v(Utils.TAG, "MAP JSON " + content);
+			JSONArray array = new JSONArray(content);
+			markers = new ArrayList<Map.CustomMarker>();
+			for (int i = 0; i < array.length(); i++) {
+				JSONObject o = array.getJSONObject(i);
+				CustomMarker m = new CustomMarker();
+				m.title		= o.getString("TITLE");
+				m.address	= o.getString("ADDRESS");
+				m.desc		= o.getString("DESC");
+				m.lat		= o.getDouble("LATITUDE");
+				m.lng		= o.getDouble("LONGITUDE");
+				m.cat		= o.getString("CATEGORY");
+				markers.add(m);
+			}
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		
+		return markers;
+	}
+
+	private String connectToDB() {
+		StringBuilder builder = new StringBuilder();
+		HttpClient client = new DefaultHttpClient();
+		HttpGet request = new HttpGet("http://nutty.rymdraket.net/android/markers.php");
+		
+		try {
+			HttpResponse response = client.execute(request);
+			StatusLine statusLine = response.getStatusLine();
+			int statusCode = statusLine.getStatusCode();
+			if(statusCode == 200) {
+				HttpEntity entity = response.getEntity();
+				InputStream content = entity.getContent();
+				BufferedReader br = new BufferedReader(new InputStreamReader(content));
+				String line;
+				while((line = br.readLine()) != null) {
+					builder.append(line);
+				}
+			} else {
+				Log.e(Utils.TAG, "MAP Failed to download JSON file");
+			}
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return builder.toString();
+	}
+
+	private void initMarkers() {
+		map.addMarker(new MarkerOptions().position(new LatLng(55.711350, 13.199730)).title("Universitetssjukhus").icon(MARKER_HOSPITAL));
+		map.addMarker(new MarkerOptions().position(new LatLng(55.712051, 13.201382)).title("BMC").icon(MARKER_MED));
+		map.addMarker(new MarkerOptions().position(new LatLng(55.705233, 13.186963)).title("Tågstation").icon(MARKER_TRAIN));
+		map.addMarker(new MarkerOptions().position(new LatLng(55.724125, 13.203002)).title("ICA Fäladen").icon(MARKER_STORE));
+		map.addMarker(new MarkerOptions().position(new LatLng(55.720420, 13.201520)).title("McDonalds").icon(MARKER_FASTFOOD));
+	}
+
+	private void initMarkerIcons() {
+		markMap = new HashMap<String, BitmapDescriptor>();
+		markMap.put("HOSPITAL", BitmapDescriptorFactory.fromResource(R.drawable.marker_hospital));
+		markMap.put("MED", BitmapDescriptorFactory.fromResource(R.drawable.marker_med));
+		markMap.put("TRAIN", BitmapDescriptorFactory.fromResource(R.drawable.marker_train));
+		markMap.put("STORE", BitmapDescriptorFactory.fromResource(R.drawable.marker_store));
+		markMap.put("FASTFOOD", BitmapDescriptorFactory.fromResource(R.drawable.marker_fastfood));
+	}
+
 	private void targetMe(Location myLoc) {
 		LatLng latLng = new LatLng(myLoc.getLatitude(), myLoc.getLongitude());
 		CameraPosition camPos = new CameraPosition.Builder().target(latLng).zoom(13).build();
 		map.animateCamera(CameraUpdateFactory.newCameraPosition(camPos));
+	}
+	
+	class CustomMarker {
+		String title;
+		String address;
+		String desc;
+		double lat; 
+		double lng;
+		String cat;
 	}
 }
