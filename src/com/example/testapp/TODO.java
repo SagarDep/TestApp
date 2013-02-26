3d565d30a5eec3da40b24a6f271b2548e5cca25e
Pagge <Patrick.Ivarsson@gmail.com>
2013-02-26 02:28:58 +0100

    TODO

--- src/com/example/testapp/TODO.java
+++ src/com/example/testapp/TODO.java
@@ -18,8 +18,12 @@ public class TODO {
 	 	TODO: Lägg in skugga under top_bar
 	 	FIXME: Se om det går att specialbehandla den sista nyhets rown så att den får lite längre avstånd när man scrollat ända ner.
 	 	
+	 	
+	 	
 	 	TODO: Testa Schema mer nogrant efter refaktoriseringen
 	 	TODO: Gör samma refaktorisering av Nyheter
+	 	TODO: Lägg till fler loggmeddelande i Calendar
+	 	
 	 	
 	 	FIXME: Byta ut uppdatera knappen i Top Bar, ser inte ut som en knapp atm
 	 	
